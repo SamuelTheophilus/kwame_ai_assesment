bad1eadb47ebbb0f9abbbed88eed93ba9227455f
SamuelTheophilus <ansahsamueltheophilus@gmail.com>
2023-09-29 00:34:23 +0000

    Api to fetch passages when question is asked successfully implemeneted

--- app/app.py
+++ app/app.py
@@ -2,13 +2,14 @@ from flask import Flask, request, jsonify
 from indexing import index_name
 from model import model
 from retrieval import retrieve_passages
-app = Flask(__name__)
 
+app = Flask(__name__)
 
 
 @app.get("/")
 def home():
     return "Home"
+
 
 @app.post('/api/question')
 def receive_question():
@@ -19,14 +20,13 @@ def receive_question():
             return jsonify({'error': 'Missing question'}), 400
 
         # Retrieve relevant passages using your passage retrieval logic
-        passages = retrieve_passages(index_name, question, top_k = 3, model = model)
+        passages, _, _ = retrieve_passages(index_name, question, top_k=3, model=model)
 
         # Return the passages as JSON response
         return jsonify({'passages': passages}), 200
 
     except Exception as e:
         return jsonify({'error': str(e)}), 500
-
 
 
 if __name__ == "__main__":
--- app/model.py
+++ app/model.py
@@ -11,17 +11,18 @@ if __name__ == "__main__":
     project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
     passage_metadata_path = os.path.join(project_root, "docs", "passage_metadata.csv")
 
-    with open(passage_metadata_path, "r", encoding = "utf-8") as file:
+    with open(passage_metadata_path, "r", encoding="utf-8") as file:
         csv_reader = csv.reader(file)
         next(csv_reader, None)
-        
+
         passage_metadata_emb_path = os.path.join(project_root, "docs", "passage_metadata_emb.csv")
-        with open(passage_metadata_emb_path, "a", encoding = "utf-8",newline ='') as csv_file:
-            headers = ["Passage", "Metadata","Embeddings"]
-            writer = csv.DictWriter(csv_file,fieldnames= headers)
+        with open(passage_metadata_emb_path, "a", encoding="utf-8", newline='') as csv_file:
+            headers = ["Passage", "Metadata", "Embeddings"]
+            writer = csv.DictWriter(csv_file, fieldnames=headers)
             writer.writeheader()
-        
+
             for row in csv_reader:
                 passage, metadata = row
                 embedding = model.encode(passage)
-                writer.writerow({ "Passage": passage, "Metadata":  metadata, "Embeddings": json.dumps(embedding.tolist())})      
+                writer.writerow(
+                    {"Passage": passage, "Metadata": metadata, "Embeddings": json.dumps(embedding.tolist())})
